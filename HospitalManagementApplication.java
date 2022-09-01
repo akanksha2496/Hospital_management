87ce9bb09ae5ec5f2eb76b7d7c8d2e1138807771
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2022-09-01 09:44:38 +0530

    Add files via upload

---
+++ HospitalManagementApplication.java
@@ -0,0 +1,27 @@
+package com.hospital.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Topic cover under this project.
+//1.Spring boot - basic concept.
+//2.dependecy Injection
+//3. Autowiring
+//4. Controller
+//5. service.
+//6. Repository
+//7.JPA:
+//	CRUD repo - vo sab JPA k andar ata.
+//8. DB- MY SQL
+//9.one to many- exmple
+//10. REST
+//11. MVC
+
+@SpringBootApplication
+public class HospitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementApplication.class, args);
+	}
+
+}
